ecd5d08c8f7fe42e8466f6ce1299c614b6df15cb
Railson Monteiro Vieira <railson190519@gmail.com>
2024-09-23 16:41:12 -0300

    aulas de 23/09/2024

---
+++ PHP_DB/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Acessando Banco de Dados</title>
+</head>
+<body>
+    <?php
+       $db_servidor = "localhost";
+       $db_nome = "lista de compras";
+       $db_user = "root";
+       $db_senha = "";
+
+       try {
+          $conn = new PDO("mysql:host=$db_servidor;dbname=$db_nome",$db_user,$db_senha);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+          echo "Acessando: $db_nome";
+
+          $stmt = $conn->prepare("SELECT * FROM lista");
+          $stmt->execute();
+          echo "<br><ul>";
+          foreach ($stmt as $linha) {
+            
+            echo "<li>";
+            echo "<a href='listaitem.php?lista=".$linha["codigo"]."&nome=".$linha["nome"]."'>".$linha["codigo"]." - ".$linha["nome"]."</a>";
+            echo "</li>";
+          }
+          echo "</ul>";
+
+       } catch (PDOException $e) {
+           echo "Erro ao conectar no Banco de Dados <br>".$e->getMessage(); 
+       }
+    ?>
+</html>
---
+++ PHP_Lista/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Compras</title>
+    <style>
+        section{
+            width: 500px;
+            height: 250px;
+        }
+        .cantainer{
+            background-color: #A8C7BB;
+        }
+    </style>
+</head>
+<body>
+    <section>
+        <div class="container">
+            <h1>Lista de Compras</h1>
+            <?php
+            require_once('conexao.php');
+            try {
+                $stmt = $conn->prepare("SELECT * FROM lista");
+                $stmt->execute();
+                foreach ($stmt as $linha) {            
+                    echo "<details>";
+                    echo "<summary>".$linha["codigo"]." - ".$linha["nome"]."</summary>";
+                        $stmtitem = $conn->prepare("SELECT * FROM item where codigo_lista = ".$linha["codigo"]);
+                        $stmtitem->execute();
+                        echo "<ul>";
+                        foreach ($stmtitem as $linhaitem) {         
+                        echo "<li>";
+                        echo $linhaitem["codigo"]." - ".$linhaitem["descricao"]." - ".$linhaitem["quantidade"]." - ".$linhaitem["datahora"]." - ".$linhaitem["codigo_lista"];
+                        echo "</li>";
+                        }
+                        echo "</ul>";
+                    echo "</details>";
+                }
+            } catch (PDOException $e) {
+                echo "Erro ao conectar no Banco de Dados <br>".$e->getMessage(); 
+            }
+            ?>
+        </div>
+    </section>
+</html>
